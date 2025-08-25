172a9a51fc94bd9c07a947f0413ea8e1e6351c9d
AhmedAlzubaidi <a.alzubaidi@arbn.sa>
2025-08-25 09:02:54 +0300

    refactor: app boot configurations.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,23 +5,57 @@ declare(strict_types=1);
 namespace App\Providers;
 
 use App\Models\User;
+use Carbon\CarbonImmutable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Date;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 final class AppServiceProvider extends ServiceProvider
 {
     /**
-     * Register any application services.
-     */
-    public function register(): void
-    {
-        //
-    }
-
-    /**
      * Bootstrap any application services.
      */
     public function boot(): void
+    {
+        $this->configureCommands();
+        $this->configureModels();
+        $this->configureDates();
+        $this->configureGates();
+    }
+
+    /**
+     * Configure the application's commands.
+     */
+    private function configureCommands(): void
+    {
+        DB::prohibitDestructiveCommands(
+            app()->isProduction()
+        );
+    }
+
+    /**
+     * Configure the dates.
+     */
+    private function configureDates(): void
+    {
+        Date::use(CarbonImmutable::class);
+    }
+
+    /**
+     * Configure the models.
+     */
+    private function configureModels(): void
+    {
+        Model::shouldBeStrict(!app()->isProduction());
+    }
+
+    /**
+     * Configure the gates.
+     */
+    private function configureGates(): void
     {
         Gate::before(function (User $user, string $ability): ?bool {
             if ($user->isAdmin()) {
